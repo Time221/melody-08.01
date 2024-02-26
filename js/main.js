00608cf6878a0ffcbfbdce0da7e8a2ae58a06954
Time221 <135141599+Time221@users.noreply.github.com>
2024-02-26 12:01:41 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 $(document).ready(function () {
     var currentFloor = 2;
-    var counterUp = $(".couner-up")
-    $(".home-image path").on('mouseover', function () {
+    var floorPath = $(".home-image path");
+    var counterUp = $(".counter-button.couner-arrow-up");
+    var counterDown = $(".counter-button.couner-arrow-down");
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+
+    floorPath.on('mouseover', function () {
         currentFloor = $(this).attr("data-floor");
         $('.counter').text(currentFloor);
     });
 
+    floorPath.on('click', togglemodal);
+
+    modalCloseButton.on("click", togglemodal);
+
     counterUp.on("click", function() {
-        currentFloor++;
-        usCurentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false});
-        $('.counter').text(usCurentFloor);
-    })
+        if (currentFloor < 18) {
+            currentFloor++;
+            var usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+            $('.counter').text(usCurrentFloor);
+            floorPath.removeClass('current-floor');
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+        }
+    });
+    counterDown.on("click", function() {
+        if (currentFloor > 2) {
+            currentFloor--;
+            var usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+            $('.counter').text(usCurrentFloor);
+            floorPath.removeClass('current-floor');
+            $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+        }
+    });
+    function togglemodal () {
+        modal.toggleClass("is-open");
+    }
 });
